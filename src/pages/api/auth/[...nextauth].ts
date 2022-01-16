da7c1173af2dcae276c5363fdad01f8c8dc640ff
Alfredo Paes <alfredo.alfpaes@gmail.com>
2022-01-16 15:42:47 -0300

    :sparkles: feat: user creation and login by fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb"
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from "../../../services/fauna"
 
 export default NextAuth({
     // Configure one or more authentication providers
@@ -15,4 +19,36 @@ export default NextAuth({
         }),
         // ...add more providers here
     ],
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            const { email } = user
+            try {
+                await fauna.query(
+                    q.If(// SE
+                        q.Not(//NÃO
+                            q.Exists(//EXISTE
+                                q.Match(// Exato, seja oque procura....
+                                    q.Index('user_by_email'), //Index para fazer a busca
+                                    q.Casefold(user.email) //Casefold = tratar lower-case
+                                )
+                            )
+                        ),
+                        q.Create( //Cria um novo usuário
+                            q.Collection('users'),
+                            { data: { email } }
+                        ),
+                        q.Get(//Busque o usuário correspondente
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(user.email)
+                            )
+                        )
+                    )
+                )
+                return true
+            } catch {
+                return false
+            }
+        },
+    }
 })
